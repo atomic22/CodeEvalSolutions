1168ff65bf0d61fcfc66970ddc74b963950bd2d5
Thompson <ctq9685@CMUTUAL.com>
2015-12-04 15:27:23 -0600

    First Commit

---
+++ MTH_Solution/Mth/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Mth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText("test.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (line != null)
+                    {
+
+                        List<string> characters = line.Split(' ').ToList();
+                        int index;
+                        Int32.TryParse(characters[characters.Count - 1], out index);
+
+                        characters.Reverse();
+                        if (index < characters.Count)
+                        {
+                            Console.Out.WriteLine(characters[index]);
+                        }
+                        
+
+                    }
+
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Stack Implementation/Stack Implementation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Stack_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText("test.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    List<int> nums = new List<int>();
+                    string[] numbers = line.Split(' ');
+                    int num;
+                    
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        Int32.TryParse(numbers[i], out num);
+                        nums.Add(num);
+                    }
+                    
+                    nums.Reverse();
+
+                    for (int i = 0; i < nums.Count; i=i+2)
+                    {
+
+                        Console.Out.Write(nums[i]);
+
+                        if (i<nums.Count)
+                        {
+                            Console.Out.Write(" ");
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+                Console.Read();
+            }
+        }
+    }
+}
